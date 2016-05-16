ccbb6bebb939a607d38de1a3f6471e11a027f485
gerson22 <gerson.isaias.rivera@gmail.com>
2016-05-16 13:33:51 -0500

    Resolvi conflictos en fotoPerfil.js

--- assets/js/js/fotosPerfil.js
+++ assets/js/js/fotosPerfil.js
@@ -4,21 +4,20 @@ $(document).ready(function(){
 		'url' : '/fotoPerfil/getAllUser/get',
 		'type' : 'POST',
 		'dataType' : 'JSON'
-	})
-	.done(function(response){
-		$.each(response, function(index, objeto){
-			$(".img-user").css({
-				'background' : "url(../../../"+objeto.imagenPortada+")",
-				'background-size' : 'cover',
-				'background-position' : 'center'
-			});
+	}).done(function(response){
+		$(".img-user").css({
+			'background' : "url(../../../"+response['fotoPortada'][0].imagenPortada+")",
+			'background-size' : 'cover',
+			'background-position' : 'center'
 		});
+		$("#imgPerfil").attr('src', '/'+response['fotoPerfil'][0].imagenPerfil);
 	});
 
 
 	$("#changePortada").click(function(){
 		$("#filePerfil").trigger('click');
 	});
+
 
 	$("#filePerfil").change(function(){
 		var formData = new FormData($("#formPerfil")[0]);
@@ -40,6 +39,38 @@ $(document).ready(function(){
 					'background-size' : 'cover',
 					'background-position' : 'center'
 				});
+				alertify.success('Imagen de portada actualizada');
+			}
+			else if(response.status == 'invalid'){
+				alertify.error('Error, el archivo no es una imagen valida');
+			}
+		})
+		.always(function(){
+
+		});
+		$(this).val('');
+	});
+
+});
+	$("#changePerfil").click(function(){
+		$("#filePerfilCircle").trigger('click');
+	});
+
+	$("#filePerfilCircle").change(function(){
+		var formData = new FormData($("#formPerfilCircle")[0]);
+
+		$.ajax({
+			url : '/fotoPerfil/imagenPerfilSave/save',
+			type : 'POST',
+			dataType : 'JSON',
+			data: formData,
+			cache: false,
+			contentType: false,
+			processData: false
+		})
+		.done(function(response){
+			if(response.status == 'success'){
+				$("#imgPerfil").attr('src', '/'+response.imagenRuta);
 				alertify.success('Imagen de portada actualizada');
 			}
 			else if(response.status == 'invalid'){
